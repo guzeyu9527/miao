ddaf1ad32fe8a9984248659d87f02e33b2bb1cd6
Your Nam0e <QQ@.3316517562.com>
2020-04-30 14:09:02 +0800

    lodash

---
+++ lodash/guzeyu9527_lodash.js
@@ -0,0 +1,13 @@
+var guzeyu9527 {
+
+}
+var foo ={
+    chunk: function(arr, size = 1) {
+        var x =TrieBurger.compact(arr)
+        var x = this.compact(arr)
+    },
+
+    compact: function(arr) {
+        
+    }
+}
